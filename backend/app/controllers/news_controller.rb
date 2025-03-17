a8dc29a9e4c4c24dfff808d9596b6e98c3e242a5
ikotome <kotomizu.0728@gmail.com>
2025-03-17 18:24:51 +0900

    ニュースAPIの統合: NewsControllerを追加し、APIからニュースを取得する機能を実装

--- backend/app/controllers/news_controller.rb
+++ backend/app/controllers/news_controller.rb
@@ -1,2 +1,12 @@
 class NewsController < ApplicationController
+  def index
+    uri = URI.parse('https://newsapi.org/v2/everything?country=jp&apiKey=1d0374fdf52a45c2a7e04fb816f0a4a1')
+    puts uri
+    json = Net::HTTP.get(uri)
+    moments = JSON.parse(json)
+    # @data = moments['articles'].to_json
+    render json: moments
+  end
+
 end
+
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "/news" => "news#index"
   mount GrapeSwaggerRails::Engine => '/swagger'
 end
