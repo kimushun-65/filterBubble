53ab414280cc15e491c2fd8a263b4220d39cf4fb
shunsuke <kimushun1226.634@gmail.com>
2025-03-17 23:08:36 +0900

    興味ジャンルの取得

--- frontend/feature/home/display/homeContainer.tsx
+++ frontend/feature/home/display/homeContainer.tsx
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { fetchGenresEvaluation } from '@/hooks/fetchGenresEvaluation';
+import { fetchGenre } from '@/hooks/fetchGenre';
+
 import { UserGenresEvaluation } from '@/types/userGenresEvaluation';
+import { Genre } from '@/types/genres';
+
 const HomeContainer = () => {
   const { userId } = useParams();
   const [userGenresEvaluation, setUserGenresEvaluation] = useState<
     UserGenresEvaluation[]
   >([]);
+  const [interestGenres, setInterestGenres] = useState<Genre[]>([]);
+
   useEffect(() => {
     const fetchUserGenresEvaluation = async () => {
       const userGenresEvaluation = await fetchGenresEvaluation();
-      console.log(userGenresEvaluation);
+      setUserGenresEvaluation(userGenresEvaluation);
+      const genres = await fetchGenre();
+      const interestGenres: Genre[] = [];
+      for (const userGenreEvaluation of userGenresEvaluation) {
+        if (userGenreEvaluation.evaluation === 5) {
+          const matchingGenre = genres.find(
+            (genre) => genre.id === userGenreEvaluation.genreId,
+          );
+          if (matchingGenre) {
+            interestGenres.push(matchingGenre);
+            console.log(matchingGenre);
+          }
+        }
+      }
     };
     fetchUserGenresEvaluation();
   }, [userId]);
