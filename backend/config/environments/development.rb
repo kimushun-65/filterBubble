db4b0e7ce56400bc9c307a15e364b371e9cb1555
ikotome <kotomizu.0728@gmail.com>
2025-03-19 00:49:41 +0900

    許可するホストを追加: bfbbackend.ikoto.meを設定

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # 許可するホストを追加
+  config.hosts << "bfbbackend.ikoto.me"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
